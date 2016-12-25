bac44df3e0768886b6845371785b5a632cc3cd16
shayl83 <stacey.garrison@gmail.com>
2016-12-25 13:10:49 -0500

    image uploading component added

--- components/App.js
+++ components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainWindow from './MainWindow.js';
 import Sidebar from './Sidebar.js';
+import ImageUploader from './ImageUploader';
 import style from '../scss/main.scss';
 
 export default class App extends React.Component {
@@ -8,23 +9,17 @@ export default class App extends React.Component {
         super(props);
         //this.state = { counter: 0 };
         //this.state = {value: ''};
-
-        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(event) {
-        console.log(event);
-    }
 
     render() {
-
         return (
             <div className="app-container">
                 <header className="upload-header">
                     <div className="upload-title">
                         Upload an Image
                     </div>
-                    <input type="file" className="image-uploader" onChange={this.handleChange} />
+                    <ImageUploader />
                 </header>
                 <MainWindow />
                 <Sidebar />
---
+++ components/ImageUploader.js
@@ -0,0 +1,47 @@
+/**
+ * Created by shayl on 12/25/2016.
+ */
+import React from 'react';
+
+export default class ImageUploader extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data_uri: null
+        }
+
+        this.handleFile = this.handleFile.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleFile(e) {
+        const reader = new FileReader();
+        const file = e.target.files[0];
+        reader.onload = (upload) => {
+            this.setState({
+                data_uri: upload.target.result,
+                filename: file.name,
+                filetype: file.type
+            });
+        };
+        reader.readAsDataURL(file);
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        let imageURL = this.state.data_uri;
+        console.log(imageURL);
+    }
+    render (){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Image Title</label>
+                <input type="text" />
+                <label>Create New Category</label>
+                <input type="text"/>
+                <input type="file" className="image-uploader" onChange={this.handleFile} />
+                <input type="submit" />
+            </form>
+        );
+    }
+}
+
