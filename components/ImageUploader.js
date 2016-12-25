529a48465a2d90e2264d1b86779b4b066a1aa32f
shayl83 <stacey.garrison@gmail.com>
2016-12-25 14:03:32 -0500

    image title added

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -8,10 +8,12 @@ export default class ImageUploader extends React.Component {
         super(props);
 
         this.state = {
-            data_uri: null
+            data_uri: null,
+            textValue: null
         }
 
         this.handleFile = this.handleFile.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleFile(e) {
@@ -28,27 +30,43 @@ export default class ImageUploader extends React.Component {
     }
     handleSubmit(e){
         e.preventDefault();
-        let imageURL = this.state.data_uri;
-        console.log(imageURL);
+        this.setState({
+            uploaded_uri: this.state.data_uri
+        });
+
+    }
+    handleChange(e){
+        this.setState({
+            textValue: e.target.value
+        });
     }
     render (){
         let uploaded;
-        if (this.state.data_uri){
+        let imageTitle;
+        if (this.state.uploaded_uri){
             uploaded = (
                 <div>
-                    <img className='image-preview' src={this.state.data_uri} />
+                    <img className='image-preview' src={this.state.uploaded_uri} />
                 </div>
+            );
+        }
+        if (this.state.textValue){
+            imageTitle = (
+              <div>
+                  {this.state.textValue}
+              </div>
             );
         }
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>Image Title</label>
-                <input type="text" />
+                <input type="text" onChange={this.handleChange} />
                 <label>Create New Category</label>
-                <input type="text"/>
+                <input type="text" />
                 <input type="file" className="image-uploader" onChange={this.handleFile} />
                 <input type="submit" />
-                {uploaded}
+                <div><strong>Image Title:</strong> {imageTitle}</div>
+                <div>Uploaded Image: {uploaded}</div>
             </form>
         );
     }
