228e7f6317bb55b62310740e273ab85b8cb0ee9a
shayl83 <stacey.garrison@gmail.com>
2017-02-07 20:51:50 -0500

    changing my app into a browser game cause that is more fun

--- components/NameTeam.js
+++ components/NameTeam.js
@@ -8,14 +8,28 @@ import React from 'react';
 export default class NameTeam extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({
+            value: event.target.value
+        });
+    }
+    handleSubmit(event){
+        event.preventDefault();
+        this.props.onNameTeam(this.state.value);
     }
     render(){
         return (
-          <div>
+        <form onSubmit={this.handleSubmit}>
               <div>Name your team</div>
-              <input type="text" />
-              <button>Submit</button>
-          </div>
+              <input type="text" onChange={this.handleChange} value={this.state.value} />
+              <input type="submit" value="Submit" />
+          </form>
         );
     }
 }
