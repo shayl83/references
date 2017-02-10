fcbefa7fef9007298c7e743d7d4ec0e2a7335b54
shayl83 <stacey.garrison@gmail.com>
2017-02-09 22:49:03 -0500

    Updated the game data to have constructors

--- index.js
+++ index.js
@@ -3,83 +3,56 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import style from './node_modules/node.normalize.scss/_normalize.scss';
 
-//this page just renders the actual app
-
-const fakeAppData = {
-    categories: [
-        "Animals",
-        "People",
-        "Monsters",
-        "Cartoons"
-    ],
-    subCategories: [
-        "Hands",
-        "Feet"
-    ],
-    imageData: [
-        {
-            imageID: 1,
-            mainCategory: "Animals",
-            subCategories: ["Cats", "Feet"],
-            title: "Cat Feet",
-            url:"http://lorempixel.com/400/200/cats"
-        },
-        {
-            imageID: 2,
-            mainCategory: "People",
-            subCategories: ["Female", "Face"],
-            title: "Female Human Feet",
-            url:"http://lorempixel.com/400/200/"
-        }
-
-    ],
-    defaultImage: "http://lorempixel.com/400/200/cats"
-};
-// class Team {
-//     constructor(species, attributes, players){
-//
-//     }
-// };
-
+//this page renders the actual app and passes in the top-level data (will also eventually pull in saved stuff from localstorage)
 const gameData = {
+    //title, health, strength, insanity, players
     teams: {
-        gargoyles: {
-            title:'Gargoyles',
-            health:1,
-            strength:1,
-            insanity:1
-        },
-        dragons: {
-            title:'Dragons',
-            health:1,
-            strength:1,
-            insanity:1
-        },
-        kittens: {
-            title:'Kittens',
-            health:1,
-            strength:1,
-            insanity:1
-        },
-        werewolves: {
-            title:'Werewolves',
-            health:1,
-            strength:1,
-            insanity:1
-        }
+        team1: new Team('Gargoyles', 20, 20, 5, {
+            player1:new Player('stacey',30),
+            player2:new Player('shayl',20),
+            player3:new Player('hyster',50)
+        }),
+        team2: new Team('Dragons', 30, 25, 10, {
+            player1:new Player('stacey',0),
+            player2:new Player('shayl',10),
+            player3:new Player('hyster',50)
+        }),
+        team3: new Team('Kittens', 15, 5, 30, {
+            player1:new Player('stacey',0),
+            player2:new Player('shayl',10),
+            player3:new Player('hyster',50)
+        }),
+        team4: new Team('Werewolves', 18, 35, 25, {
+            player1:new Player('stacey',0),
+            player2:new Player('shayl',10),
+            player3:new Player('hyster',50)
+        }),
     },
+    //title, light, size
     arenas: {
-        cave: {
-            darkness:1,
-            size:1
-        },
-        beach: {
-            darkness:1,
-            size:1
-        }
+        arena1: new Arena('cave', 2, 50),
+        arena2: new Arena('beach', 20, 20),
     }
+};
+//each team is also going to eventually have an array of players to choose from, which will probably have stats of their own
+//data constructors
+function Team(title, health, strength, insanity, players){
+    this.title = title;
+    this.health = health;
+    this.strenth = strength;
+    this.insanity = insanity;
+    this.players = players;
 }
-
+function Player(name, steroids){
+    this.name = name;
+    this.steroids = steroids;
+}
+function Arena(title, light, size){
+    this.title = title;
+    this.light = light;
+    this.size = size;
+}
+console.log(gameData);
 ReactDOM.render(
     <App
         gameData={gameData}
